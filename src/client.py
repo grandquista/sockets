cb03d40536493b6b3d30312fe58727df86c4356c
Adam Grandquist <grandquista@gmail.com>
2018-04-02 16:42:17 -0700

    Build out the server and client.

--- src/client.py
+++ src/client.py
@@ -9,10 +9,32 @@ running a script.  When the server sends a response back, the client should
 accumulate the response and print it to the console.
 """
 
+from socket import getaddrinfo, socket, SOCK_STREAM
+from sys import argv
+
+BUFFER_SIZE = 1024
+
 
 def main():
-    pass
+    info = getaddrinfo('127.0.0.1', 8080)
+    info = [i for i in info if i[1] == SOCK_STREAM][0]
+    with socket(*info[:3]) as client:
+        client.connect(info[-1])
+        if len(argv) > 1:
+            for msg in argv[1:]:
+                client.sendall(msg.encode('utf8'))
+                size = BUFFER_SIZE
+                while size == BUFFER_SIZE:
+                    buffer = client.recv(BUFFER_SIZE)
+                    print(buffer.decode('utf8'))
+                    size = len(buffer)
+            return
+        while True:
+            input(': ')
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
--- src/server.py
+++ src/server.py
@@ -14,20 +14,36 @@ that message. It doesn't have to match this exactly, but it should be something
 like: [12:43:10 29/01/2018] Echoed: 'Hello, world!'
 """
 
-from datetime.datetime import now
+from datetime import datetime
+from socket import socket, AF_INET, IPPROTO_TCP, SOCK_STREAM
 
+
+ADDR = ('127.0.0.1', 8080)
+
+BUFFER_SIZE = 1024
 
 SERVER_START = '--- Starting server on port {port} at {date} ---'
 SERVER_ECHO = '[{date}] Echoed: {msg}'
-SERVER_END = '--- Stopping server on port {port} at {date} ---'
+SERVER_END = '\n--- Stopping server on port {port} at {date} ---'
 
 
 def main():
-    print(SERVER_START.format(port=8888, date=now()))
     try:
-        pass
+        with socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) as server:
+            server.bind(ADDR)
+            print(SERVER_START.format(port=ADDR[1], date=datetime.now()))
+            server.listen()
+            conn, addr = server.accept()
+            with conn:
+                while True:
+                    size = BUFFER_SIZE
+                    while size == BUFFER_SIZE:
+                        buffer = conn.recv(BUFFER_SIZE)
+                        # print(buffer.decode('utf8'))
+                        conn.sendall(buffer)
+                        size = len(buffer)
     finally:
-        print(SERVER_END.format(port=8888, date=now()))
+        print(SERVER_END.format(port=ADDR[1], date=datetime.now()))
 
 
 if __name__ == '__main__':
